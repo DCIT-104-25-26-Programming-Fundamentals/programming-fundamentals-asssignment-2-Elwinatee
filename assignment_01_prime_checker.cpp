74a15dd2237d7779a838164bc68c11f60149127f
Elwinatee <egmah@st.ug.edu.gh>
2026-08-02 17:22:07 +0000

    Complete assignments 1-9

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,27 @@
 
 #include <iostream>
 using namespace std;
+bool isprime(int num) {
+  if (num < 2) { 
+    return false; 
+  }
+  for (int i = 2; i * i <= num; ++i) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+int main() {
+  int number;
+  cout << "Enter a number: ";
+  cin >> number;
 
+  if (isprime(number)) {
+    cout << number << " is a prime number." << endl;
+  } else {
+    cout << number << " is NOT a prime number." << endl;
+  }
+
+  return 0;
+}
